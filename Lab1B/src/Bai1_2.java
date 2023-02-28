939c74a546b6ca09afab26e1e27d34e7680d68a2
Nguyễn Tiến Thành <88380769+Thanhhoacam@users.noreply.github.com>
2023-02-28 20:33:33 +0700

    Lab1B

---
+++ Lab1B/src/Bai1_2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Bai1_2 {
+    public static void main(String[] args) {
+        System.out.println("CHƯƠNG TRÌNH TÍNH TỔNG CHUỖI");
+        Scanner input = new Scanner(System.in);
+
+        String str = new String("1-1/2+1/3-1/4+1/5");
+
+
+        double sum = Double.parseDouble(str);
+        System.out.println(str);
+        System.out.println(sum);
+
+//        System.out.println("Enter value of e (0<e<1) : ");
+//        double e = input.nextDouble();
+//
+//
+//       while(e<=0.0 || e>=1.0){
+//           System.out.println("Enter value of e (0<e<1) : ");
+//           e = input.nextDouble();
+//       }
+
+
+
+    }
+}
